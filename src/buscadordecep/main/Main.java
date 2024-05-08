2d968fb441851f2ec3cd9a55457747195a288f3c
Mateus Perpetuo <mateus_perpetuo@outlook.com>
2024-05-08 00:23:48 -0300

    v01 - configurações iniciais

---
+++ src/buscadordecep/main/Main.java
@@ -0,0 +1,17 @@
+package buscadordecep.main;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Criando o gson e padronizando pra tudo minusculo
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .setPrettyPrinting()
+                .create();
+
+    }
+
+}
